5a6b06841849772ef7e996caa894588028df0ba0
R4chel <rae84@cornell.edu>
2022-12-16 15:00:21 -0500

    add stars

--- sketch_vsk_hearts.py
+++ sketch_vsk_hearts.py
@@ -3,6 +3,7 @@ from shapely.geometry import Point, LinearRing
 import vpype as vp
 import numpy as np
 from point2d import Point2D
+import bisect
 
 
 def heart_f(t):
@@ -13,6 +14,13 @@ def heart_f(t):
 def heart_pts(num_points):
     thetas = [i * 2 * np.pi / num_points for i in range(num_points)]
     return [Point2D(a=theta, r=heart_f(theta)) for theta in thetas]
+
+
+def star_pts(n):
+    return [
+        Point2D(a=(i * np.pi / n), r=(1 if i % 2 == 0 else 0.5))
+        for i in range(n * 2)
+    ]
 
 
 class BoundingCircle:
@@ -41,6 +49,14 @@ class BoundingCircle:
         vsk.stroke(self.layer)
         vsk.geometry(LinearRing(points))
 
+    def draw_star(self, vsk: vsketch.SketchClass, star_points):
+        pts = [p * self.r for p in star_points]
+        for p in pts:
+            p.a += self.rotation
+        points = [Point((p + self.p).cartesian()) for p in pts]
+        vsk.stroke(self.layer)
+        vsk.geometry(LinearRing(points))
+
 
 class VskHeartsSketch(vsketch.SketchClass):
     # Sketch parameters:
@@ -52,6 +68,8 @@ class VskHeartsSketch(vsketch.SketchClass):
     max_rotation = vsketch.Param(np.pi + np.pi / 3)
     num_points = vsketch.Param(500)
     num_layers = vsketch.Param(1)
+    min_star_points = vsketch.Param(8)
+    max_star_points = vsketch.Param(8)
 
     def max_radius_at_p(self, vsk: vsketch.SketchClass,
                         circles: list[BoundingCircle], point: Point):
@@ -60,9 +78,8 @@ class VskHeartsSketch(vsketch.SketchClass):
             self.height - point.y
         ])
 
-        # opportunity to improve performance by sorting circles by radius
         for c in circles:
-            d = point.distance(c.p) - c.r
+            d = (point - c.p).r - c.r
             max_r = min(d, max_r)
             if max_r < self.min_radius:
                 break
@@ -72,7 +89,7 @@ class VskHeartsSketch(vsketch.SketchClass):
             return None
 
     def random_point(self, vsk: vsketch.Vsketch):
-        return Point(vsk.random(0, self.width), vsk.random(0, self.height))
+        return Point2D(vsk.random(0, self.width), vsk.random(0, self.height))
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a6", landscape=True)
@@ -85,8 +102,6 @@ class VskHeartsSketch(vsketch.SketchClass):
                               self.height) * self.max_radius_ratio / 2
         self.min_radius = min(self.width,
                               self.height) * self.min_radius_ratio / 2
-
-        # implement your sketch here
 
         layers = range(1, self.num_layers + 1)
         circles = []
@@ -101,7 +116,13 @@ class VskHeartsSketch(vsketch.SketchClass):
                 c = BoundingCircle(
                     p, maybe_r, layer,
                     vsk.random(self.min_rotation, self.max_rotation))
-                circles.append(c)
+
+                # keep list of circles sorted by decreasing radius for circle packing performace
+                bisect.insort(circles,
+                              c,
+                              lo=len(circles),
+                              hi=0,
+                              key=lambda c: c.r)
                 attempts = 0
             else:
                 attempts += 1
@@ -110,7 +131,11 @@ class VskHeartsSketch(vsketch.SketchClass):
         ## draw shapes
         for c in circles:
             # c.draw(vsk)
-            c.draw_heart(vsk, heart_points)
+            # c.draw_heart(vsk, heart_points)
+            num_star_points = int(
+                vsk.random(self.min_star_points, self.max_star_points + 1))
+            star_points = star_pts(num_star_points)
+            c.draw_star(vsk, star_points)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
